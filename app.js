6495433a738ec20317c10e178d79a2f938d93dbb
Jasmine Bien-Aime <82053164+Jasminethecreator@users.noreply.github.com>
2021-08-06 19:31:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -23,18 +23,6 @@ const apiData = await fechData(API_URL)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 //
 // // save job function
 // let saveButton = document.querySelector('#save-button');
@@ -82,20 +70,23 @@ searchForm.addEventListener('submit', async function (event) {
     jobData.forEach(function (job) {
         const div = document.createElement('div')
         div.innerHTML =
-           `<div class="flex flex-col justify-center text-xl  m-10 mb-2 p-3  text-sm border-2"> 
-            <p><strong>Job tittle:</strong>  ${job.title}</p>
-            <p><strong>Company:</strong> ${job.company.display_name}</p> 
-            <p><strong>Loction:</strong> ${job.location.display_name}</p>
-            <p><strong>Posted:</strong> ${job.created}</p>
-            <p class = "p-2 flex"><a href="mailto:${job.id}@thetechcove.com?subject=Hi Jasmine, view this job posting! ${job.title}">
-            <img src="icons/sent-mail.png" alt="Send email" width="30" height="30" ></a>
+            `<div class="flex flex-col justify-center text-xl  m-10 mb-2 p-3  text-sm border-4"> 
+            <p class="mb-2"> <strong>Job title:</strong>  ${job.title}</p>
+            <p class="mb-2"> <strong>Company:</strong> ${job.company.display_name}</p> 
+            <p class="mb-2"> <strong>Loction:</strong> ${job.location.display_name}</p>
+            <p class="mb-2"> <strong>Description:</strong> ${job.description}</p>
+            <p class="mb-2"> <strong>Posted:</strong> ${job.created}</p>
+            <p class="mb-2"> <a href="${job.redirect_url}">Apply to this postion</p>
+            <p class ="p-2 mb-2 flex"> <a href="mailto:${job.id}@thetechcove.com?subject=Hi Jasmine, view this job posting! ${job.title}">
+            <img src="../PhpstormProject/Sprint-6-Job-Jobs/icons/sent-mail.png" alt="Send email" width="30" height="30" ></a>
             <button class="focus:outline-none "> <svg id="save-button">height="30" viewBox="0 0 48 48" width="30" xmlns="http://www.w3.org/2000/svg"><path d="M34 6H14c-2.21 0-3.98 1.79-3.98 4L10 42l14-6 14 6V10c0-2.21-1.79-4-4-4z"/><path d="M0 0h48v48H0z" fill="none"/></svg></button>
             </p>
             </div>`
 
-    jobResultTag.appendChild(div)
+        jobResultTag.appendChild(div)
     })
 })
+
 
 
 
