4afc2f4234a92415a3ee7039b17548396929eed8
Jasmine Bien-Aime <82053164+Jasminethecreator@users.noreply.github.com>
2022-02-24 23:24:50 -0500

    upload custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    colors: {
+      blue: {
+        100: '#ebf8ff',
+        200: '#bee3f8',
+        300: '#90cdf4',
+        400: '#63b3ed',
+        500: '#4299e1',
+        600: '#3182ce',
+        700: '#2b6cb0',
+        800: '#2c5282',
+        900: '#2a4365'
+      },
+      red: {
+        100: '#fff5f5',
+        200: '#fed7d7',
+        300: '#feb2b2',
+        400: '#fc8181',
+        500: '#f56565',
+        600: '#e53e3e',
+        700: '#c53030',
+        800: '#9b2c2c',
+        900: '#742a2a'
+      }
+    }
+  }
 }
+  
